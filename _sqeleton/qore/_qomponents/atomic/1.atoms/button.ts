e79303196c473a784b2723ce431d12d8ccfb3cf1
JMelnick <jmelnick@atlantistech.com>
2022-07-26 05:39:42 -0400

    minor updates

--- _sqeleton/qore/_qomponents/atomic/1.atoms/button.ts
+++ _sqeleton/qore/_qomponents/atomic/1.atoms/button.ts
@@ -1,3 +1,5 @@
+// https://github.com/rollup/rollup-starter-lib/tree/typescript
+// // rollup this to an iife -> which is then imported from the styleguide by a normal <script> tag... maybe
 export const Button = () => {
   console.log('BUTTON')
 }
