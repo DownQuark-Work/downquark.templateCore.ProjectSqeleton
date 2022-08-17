e56899128236b38e3d3235f6501e8362ade0c4e0
mlnck <mlnck@mlncks-MacBook-Pro.local>
2022-08-17 02:38:50 -0400

    inits PWA structure

---
+++ _sqeleton/qore/web/extensions/pwa/src/_content/script.ts
@@ -0,0 +1,3 @@
+// export all injectable scripts from this file
+  // rollup process will (com/trans)pile these to:
+    // `_extension/_transpiled/content-scripts.js`
---
+++ _sqeleton/qore/web/extensions/pwa/src/app/index.ts
@@ -0,0 +1,3 @@
+// entry point for all PWA source files
+//   rollup process will (com/trans)pile these to:
+//     `_extension/_transpiled/extension.js`
