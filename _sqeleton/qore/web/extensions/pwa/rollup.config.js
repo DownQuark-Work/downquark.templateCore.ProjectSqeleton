62d1b9ebbb00c15d522b93661f51473ba7c55dbe
mlnck <mlnck@mlncks-MacBook-Pro.local>
2022-08-18 03:44:07 -0400

    transpile and showing in extension

--- _sqeleton/qore/web/extensions/pwa/rollup.config.js
+++ _sqeleton/qore/web/extensions/pwa/rollup.config.js
@@ -1,24 +1,26 @@
-// import resolve from 'rollup-plugin-node-resolve';
-// import commonjs from 'rollup-plugin-commonjs';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
 import typescript from 'rollup-plugin-typescript';
 import pkg from './package.json';
 
 export default [
 	// browser-friendly UMD build
-	// {
-	// 	// input: 'src/main.ts',
-	// 	input:'atomic/1.atoms/button.ts',
-	// 	output: {
-	// 		name: 'dq',
-	// 		file: pkg.browser,
-	// 		format: 'umd'
-	// 	},
-	// 	plugins: [
-	// 		resolve(),   // so Rollup can find `ms`
-	// 		commonjs(),  // so Rollup can convert `ms` to an ES module
-	// 		typescript() // so Rollup can convert TypeScript to JavaScript
-	// 	]
-	// },
+	{
+		// input: 'src/main.ts',
+		// input:'atomic/1.atoms/button.ts',
+		input:'src/app/index.ts',
+		output: {
+			name: 'dq',
+			file: pkg.transpile,
+			// file: '_extension/TRANSPILED/dq.pwa.umd.js',
+			format: 'umd'
+		},
+		plugins: [
+			resolve(),   // so Rollup can find `ms`
+			commonjs(),  // so Rollup can convert `ms` to an ES module
+			typescript() // so Rollup can convert TypeScript to JavaScript
+		]
+	},
 
 	// CommonJS (for Node) and ES module (for bundlers) build.
 	// (We could have three entries in the configuration array
@@ -26,15 +28,15 @@ export default [
 	// builds from a single configuration where possible, using
 	// an array for the `output` option, where we can specify 
 	// `file` and `format` for each target)
-	{
-		input:'src/app/index.ts',
-		external: ['lit','lit/decorators.js'],
-		plugins: [
-			typescript() // so Rollup can convert TypeScript to JavaScript
-		],
-		output: [
-			// { file: pkg.main, format: 'cjs' },
-			{ file: pkg.transpile, format: 'es' }
-		]
-	}
+	// {
+	// 	input:'src/app/index.ts',
+	// 	external: ['lit','lit/decorators.js'],
+	// 	plugins: [
+	// 		typescript() // so Rollup can convert TypeScript to JavaScript
+	// 	],
+	// 	output: [
+	// 		// { file: pkg.main, format: 'cjs' },
+	// 		{ file: pkg.transpile, format: 'es' }
+	// 	]
+	// }
 ];
--- _sqeleton/qore/web/extensions/pwa/src/app/index.ts
+++ _sqeleton/qore/web/extensions/pwa/src/app/index.ts
@@ -4,7 +4,9 @@
 import { DQ_PWA } from './dq.pwa'
 import { MyElement } from '../_components/dq.qomponents.esm'
 
-(document.getElementById('extension') as HTMLDivElement).innerHTML = `HTML has been updated with an imported typescript file: ${DQ_PWA}`
+(document.getElementById('extension') as HTMLDivElement).innerHTML = `HTML has been updated with an imported typescript file: ${DQ_PWA}
+<p>TODO: RENDER THIS WITH LIT AS OPPOSED TO innerHTML and include the duped my-element file
+`
 console.log('MyElement', MyElement)
 
 // <my-element>
