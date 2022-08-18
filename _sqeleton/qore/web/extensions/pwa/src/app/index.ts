4003343cad15be9d6cf10e28e4000d03d02634cf
mlnck <mlnck@mlncks-MacBook-Pro.local>
2022-08-18 03:32:34 -0400

    successful rollup to transpile dir

--- _sqeleton/qore/web/extensions/pwa/src/app/index.ts
+++ _sqeleton/qore/web/extensions/pwa/src/app/index.ts
@@ -1,6 +1,13 @@
-import { DQ_PWA } from "./dq.pwa";
+// import {LitElement, html} from 'lit';
+// import {customElement} from 'lit/decorators.js';
+
+import { DQ_PWA } from './dq.pwa'
+import { MyElement } from '../_components/dq.qomponents.esm'
 
 (document.getElementById('extension') as HTMLDivElement).innerHTML = `HTML has been updated with an imported typescript file: ${DQ_PWA}`
+console.log('MyElement', MyElement)
+
+// <my-element>
 
 // entry point for all PWA source files
 //   rollup process will (com/trans)pile these to:
