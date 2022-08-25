46d52e8acc73134e4e7b6ac9450714f31ca7207a
mlnck <mlnck@downquark.work>
2022-08-25 13:36:23 -0400

    removes outdated

--- _sqeleton/qore/persistent/relational/mariadb/dump-dq.relational-202208251335.sql
+++ _sqeleton/qore/persistent/relational/mariadb/dump-dq.relational-202208251335.sql
@@ -271,4 +271,4 @@ SET character_set_client = @saved_cs_client;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-08-25 11:46:00
+-- Dump completed on 2022-08-25 13:35:31
